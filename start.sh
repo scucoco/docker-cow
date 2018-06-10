5037e33d43ae179959aba8afabb3cb6935e76bec
scucoco <scucoco@users.noreply.github.com>
2018-06-10 17:19:40 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,6 +6,8 @@ if [ ! -f "$cowConfFile" ]; then
 	echo listen = http://$LISTEN_IP:$LISTEN_PORT >> /cowconfig/rc
 	echo proxy = ss://$SS_ENCRYPT_METHOD:$SS_PASSWORD@$SS_SERVER_IP:$SS_SERVER_PORT >> ~/.cow/rc
 	echo proxy = ss://$SS_ENCRYPT_METHOD:$SS_PASSWORD@$SS_SERVER_IP:$SS_SERVER_PORT >> /cowconfig/rc
+	echo userPasswd = $USER:$PASSWORD >> ~/.cow/rc
+	echo userPasswd = $USER:$PASSWORD >> /cowconfig/rc
 fi
 ./cow
 /bin/bash
